4fcce68b999c15b5c9d4b47320d2790cb8997721
Yasmin Ayuni <ysmnayn04@gmail.com>
2025-04-04 11:25:55 +0800

    Add on app styles source code

---
+++ lib/base/res/styles/app_styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Color primary = const Color(0xFF687daf);
+
+class AppStyles {
+  static Color primaryColor = primary;
+  static Color textColor =  const Color(0xFF3b3b3b);
+
+  static TextStyle headLineStyle1 = TextStyle(
+      fontSize: 26,
+      fontWeight: FontWeight.w500,
+      color : textColor);
+
+  static TextStyle headLineStyle3 = const TextStyle(
+      fontSize: 17,
+      fontWeight: FontWeight.w500,
+      );
+}
